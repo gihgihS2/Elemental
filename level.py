bc912ee7c214107abee480293d2e896e28032214
Renan-majin1 <aac.renanaparecido@gmail.com>
2025-11-11 09:14:58 -0300

    colocando player

--- level.py
+++ level.py
@@ -1,13 +1,25 @@
 import pygame
+from player import Player
 
 class Level:
     def __init__(self, surface):
         self.display_surface = surface
 
-        # Carregar o mapa (coloque o caminho correto até o PNG)
+        # Carregar o mapa
         self.background = pygame.image.load('assets/DESERTO/MAPAOK.png').convert()
-        self.background = pygame.transform.scale(self.background, (800, 600))  # ajusta ao tamanho da tela
+        self.background = pygame.transform.scale(self.background, (800, 600))
+
+        # Grupo de sprites
+        self.all_sprites = pygame.sprite.Group()
+
+        # Adicionar o jogador
+        self.player = Player((750, 550))  # posição inicial
+        self.all_sprites.add(self.player)
 
     def run(self, dt):
         # Desenhar o mapa de fundo
         self.display_surface.blit(self.background, (0, 0))
+
+        # Atualizar e desenhar sprites
+        self.all_sprites.update(dt)
+        self.all_sprites.draw(self.display_surface)
